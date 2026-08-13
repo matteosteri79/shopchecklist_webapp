8c3f928b6a99b7c3da3ac9854c5ff11ef7e939bb
Matteo Steri <matteo.steri@gmail.com>
2026-08-13 07:43:58 +0200

    initial commit

---
+++ assets/js/utils.js
@@ -0,0 +1,5 @@
+
+export const APP_NAME = "Shop Checklist";
+export const APP_VERSION = "1.7.2"
+export const SUPPORT_EMAIL = "matteosteri+shopchecklist@gmail.com";
+export const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.matteosteri.shopchecklist";
